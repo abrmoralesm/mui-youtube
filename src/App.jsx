d88b62d82d3f8dd7e29fa85c5ea0dba341c33849
abrmoralesm <abrmoralesm@gmail.com>
2024-04-02 21:08:54 +0200

    instalación

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { Button} from "@mui/material"
+
+export default function App() {
+  return (
+    <>
+      <h1>App</h1>
+      <Button variant="contained"> Mi primer botón</Button>
+    </>
+  );
+}
