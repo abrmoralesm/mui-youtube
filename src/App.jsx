bd1c6f442a9468aa78a8131c9371e14ed2dd2fdc
abrmoralesm <abrmoralesm@gmail.com>
2024-04-03 16:34:19 +0200

    Introduccion a Theming

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
-import { Button, Container } from "@mui/material";
-import { Box } from "@mui/system";
+import { Button, Container, Typography } from "@mui/material";
 
 export default function App() {
   return (
-    <Container sx={{ border: 3, boxShadow: 3, pb: 2 }}>
+    <Container>
       <h1>App</h1>
-      <Box
-        sx={{
-          border: 2,
-          p: 5,
-          borderColor: "peru",
-          bgcolor: "#111",
-          color: "white",
-        }}
-        component="span"
-      >
-        Pero que hermoso es MUI
-      </Box>
+<Typography variant="h3" color="primary">H3 título</Typography>
       <Button variant='contained'> Mi primer botón</Button>
     </Container>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { CssBaseline } from "@mui/material";
+import "./index.css";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 
 
 import "@fontsource/roboto/300.css";
@@ -10,10 +11,23 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
 import "./index.css";
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#004d40",
+    },
+    secondary: {
+      main: "#b500d6",
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CssBaseline />
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
