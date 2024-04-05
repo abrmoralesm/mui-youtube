b795ba0f0dba6b36fa7ad3ddaf14375b89f1196c
abrmoralesm <abrmoralesm@gmail.com>
2024-04-05 13:38:33 +0200

    Botones e iconos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import { Button, Container, Typography } from "@mui/material";
+import AdbIcon from "@mui/icons-material/Adb";
+import AcUnitIcon from "@mui/icons-material/AcUnit";
 
 export default function App() {
   return (
     <Container>
       <h1>App</h1>
-<Typography variant="h3" color="primary">H3 título</Typography>
-      <Button variant='contained'> Mi primer botón</Button>
+      <Typography variant='h3' color='primary'>
+        H3 título
+      </Typography>
+      <Button variant='contained' color='error' startIcon={<AdbIcon/>}>
+        {" "}
+        Mi primer botón
+      </Button>
+      <Button variant='outlined' color='success'endIcon={<AcUnitIcon/>}>
+        {" "}
+        Mi primer botón
+      </Button>
     </Container>
   );
 }
